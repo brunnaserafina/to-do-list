6d6d611cce78fa444190b8cc3e7387c948bb5833
Brunna Serafina <brunnaserafina@gmail.com>
2022-12-12 11:45:55 -0300

    feat & style: adding initial settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import "../src/ResetCss.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header />
+    </Container>
   );
 }
 
-export default App;
+const Container = styled.div`
+  text-align: center;
+  background: linear-gradient(to right, #4568dc, #b06ab3);
+  height: 100vh;
+`;
+
+const Header = styled.div`
+  height: 50px;
+  width: 100%;
+  background: #0052d4;
+`;
